395b2175b197945ec3061546fe5d20662eb1fd69
md-hasan-ali <hasanaliringku@gmail.com>
2024-03-08 09:55:15 +0600

    k6 load test script

---
+++ script.js
@@ -0,0 +1,19 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+    vus: 2,
+    duration: '2s',
+};
+
+export default function () {
+    const res = http.get('https://jsonplaceholder.typicode.com/users');
+
+    check(res, {
+        'status was 200': (r) => r.status == 200,
+        'responseBody Name Check': (r) => r.body.includes('Ervin Howell'),
+        'lat check': (r) => r.body.includes('-37.3159'),
+        'Lng check': (r) => r.body.includes('81.1496'),
+    });
+    sleep(1);
+}
